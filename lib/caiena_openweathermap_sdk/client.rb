d6998823ac79a09895c081332c42aeccf67074ed
petersonbem <petersondaronch@icloud.com>
2025-09-29 19:25:52 -0300

    provide averaged forecast

--- lib/caiena_openweathermap_sdk/client.rb
+++ lib/caiena_openweathermap_sdk/client.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
+require "date"
+
 module CaienaOpenweathermapSdk
   class OpenweathermapError < StandardError; end
+
   class ClientError < StandardError; end
 
   # Client to request the Open Weather API service
@@ -29,6 +32,26 @@ module CaienaOpenweathermapSdk
       raise OpenweathermapError, body["message"] || "unknown error" unless response.is_a? Net::HTTPSuccess
 
       body["main"]["temp"]
+    end
+
+    # forecast reads the forecast data from open weather
+    # @param location should be string <city>
+    # @param appid should be a access key generated at open weather
+    # @return average forecast by day for the given location
+    def forecast(location, appid)
+      uri = URI.parse("#{@base_url}/data/2.5/forecast?q=#{location}&units=metric&appid=#{appid}")
+
+      begin
+        response = Net::HTTP.get_response(uri)
+        body = JSON.parse(response.body)
+      rescue StandardError => e
+        raise ClientError, e.message
+      end
+
+      raise OpenweathermapError, body["message"] || "unknown error" unless response.is_a? Net::HTTPSuccess
+
+      body["list"].group_by { |f| Date.parse(f["dt_txt"]).to_date.to_s }
+                  .transform_values { |entries| (entries.sum { |e| e["main"]["temp"] } / entries.size).round(2) }
     end
   end
 end
--- spec/caiena_openweathermap_sdk/client_spec.rb
+++ spec/caiena_openweathermap_sdk/client_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe CaienaOpenweathermapSdk::Client do
       end
 
       it "return current temperature for the given location" do
-        expect(current_temperature).to be_a Float
+        expect(current_temperature).to eq 24.11
       end
     end
 
@@ -45,6 +45,77 @@ RSpec.describe CaienaOpenweathermapSdk::Client do
 
       it "raise the error" do
         expect { current_temperature }.to raise_error CaienaOpenweathermapSdk::ClientError, /mocked error/i
+      end
+    end
+  end
+
+  describe "#forecast" do
+    let(:params) { { q: "anywhere", appid: "anything", units: "metric" } }
+    let(:forecast) { client.forecast params[:q], params[:appid] }
+
+    let(:five_day_forecast) { "#{base_uri}/data/2.5/forecast" }
+
+    context "when open weather respond successfully" do
+      let(:response_body_mock) do
+        {
+          "list" => [
+            { "dt" => 1_759_179_600, "dt_txt" => "2025-09-29 21:00:00", "main" => { "temp" => 14.19 } },
+            { "dt" => 1_759_190_400, "dt_txt" => "2025-09-30 00:00:00", "main" => { "temp" => 13.11 } },
+            { "dt" => 1_759_244_400, "dt_txt" => "2025-09-30 15:00:00", "main" => { "temp" => 19.53 } },
+            { "dt" => 1_759_276_800, "dt_txt" => "2025-10-01 00:00:00", "main" => { "temp" => 13.76 } },
+            { "dt" => 1_759_287_600, "dt_txt" => "2025-10-01 03:00:00", "main" => { "temp" => 12.85 } },
+            { "dt" => 1_759_298_400, "dt_txt" => "2025-10-01 06:00:00", "main" => { "temp" => 12.4 } },
+            { "dt" => 1_759_363_200, "dt_txt" => "2025-10-02 00:00:00", "main" => { "temp" => 13.65 } },
+            { "dt" => 1_759_374_000, "dt_txt" => "2025-10-02 03:00:00", "main" => { "temp" => 12.82 } },
+            { "dt" => 1_759_384_800, "dt_txt" => "2025-10-02 06:00:00", "main" => { "temp" => 12.18 } },
+            { "dt" => 1_759_395_600, "dt_txt" => "2025-10-02 09:00:00", "main" => { "temp" => 15.51 } },
+            { "dt" => 1_759_406_400, "dt_txt" => "2025-10-02 12:00:00", "main" => { "temp" => 19.8 } },
+            { "dt" => 1_759_417_200, "dt_txt" => "2025-10-02 15:00:00", "main" => { "temp" => 18.33 } },
+            { "dt" => 1_759_428_000, "dt_txt" => "2025-10-02 18:00:00", "main" => { "temp" => 16.33 } },
+            { "dt" => 1_759_438_800, "dt_txt" => "2025-10-02 21:00:00", "main" => { "temp" => 16.16 } },
+            { "dt" => 1_759_449_600, "dt_txt" => "2025-10-03 00:00:00", "main" => { "temp" => 16.5 } },
+            { "dt" => 1_759_579_200, "dt_txt" => "2025-10-04 12:00:00", "main" => { "temp" => 14.37 } }
+          ]
+        }
+      end
+
+      before(:each) do
+        stub_request(:get, five_day_forecast).to_return_json(body: response_body_mock, status: 200).with(query: params)
+      end
+
+      it "return forecast by day for the given location" do
+        expect(forecast).to eq({
+                                 "2025-09-29" => 14.19,
+                                 "2025-09-30" => 16.32,
+                                 "2025-10-01" => 13.00,
+                                 "2025-10-02" => 15.60,
+                                 "2025-10-03" => 16.50,
+                                 "2025-10-04" => 14.37
+                               })
+      end
+    end
+
+    context "when open weather respond with error" do
+      before(:each) do
+        stub_request(:get, five_day_forecast)
+          .to_return_json(body: { message: "mocked error" }, status: 500)
+          .with(query: params)
+      end
+
+      it "raise the error" do
+        expect { forecast }.to raise_error CaienaOpenweathermapSdk::OpenweathermapError, /mocked error/i
+      end
+    end
+
+    context "when the request to open weather fails" do
+      before(:each) do
+        stub_request(:get, five_day_forecast)
+          .to_raise(StandardError.new("mocked error"))
+          .with(query: params)
+      end
+
+      it "raise the error" do
+        expect { forecast }.to raise_error CaienaOpenweathermapSdk::ClientError, /mocked error/i
       end
     end
   end
